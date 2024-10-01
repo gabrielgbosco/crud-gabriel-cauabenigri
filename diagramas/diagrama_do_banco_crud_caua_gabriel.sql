6e1d44464c47a0bf5b53afb5931b9ed504b6a7da
Gabriel Bosco <160626981+gabrielgbosco@users.noreply.github.com>
2024-10-01 11:15:56 -0300

    Fazendo upload dos diagramas do banco de dados

---
+++ diagramas/diagrama_do_banco_crud_caua_gabriel.sql
@@ -0,0 +1,19 @@
+create database crud_caua_gabriel;
+
+use crud_caua_gabriel;
+
+create table usuarios (
+	pk_usuario int primary key not null auto_increment,
+    nome_usuario varchar(100),
+    email_usuario varchar(100)
+);
+
+create table notas (
+	pk_nota int primary key not null auto_increment,
+    titulo_nota varchar(50),
+    conteudo_nota text,
+    data_nota timestamp default current_timestamp,
+    prioridade_nota enum('irrelevante','baixa','média','alta','urgente'),
+    fk_usuario int,
+    FOREIGN KEY (fk_usuario) REFERENCES usuarios(pk_usuario)
+);
