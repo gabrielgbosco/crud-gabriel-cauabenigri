34e9286f90f10fdc853c8f89e75b16bc7dd4082f
Gabriel Bosco <160626981+gabrielgbosco@users.noreply.github.com>
2024-10-08 08:33:28 -0300

    Update diagrama_do_banco_crud_caua_gabriel.sql

--- diagramas/diagrama_do_banco_crud_caua_gabriel.sql
+++ diagramas/diagrama_do_banco_crud_caua_gabriel.sql
@@ -1,19 +1,9 @@
-create database crud_caua_gabriel;
+create database sistema_bloco_notas_caua;
 
-use crud_caua_gabriel;
+use sistema_bloco_notas_caua;
 
-create table usuarios (
-	pk_usuario int primary key not null auto_increment,
-    nome_usuario varchar(100),
-    email_usuario varchar(100)
-);
-
-create table notas (
-	pk_nota int primary key not null auto_increment,
-    titulo_nota varchar(50),
-    conteudo_nota text,
-    data_nota timestamp default current_timestamp,
-    prioridade_nota enum('irrelevante','baixa','média','alta','urgente'),
-    fk_usuario int,
-    FOREIGN KEY (fk_usuario) REFERENCES usuarios(pk_usuario) ON DELETE CASCADE
+create table mensagens (
+    id int auto_increment primary key ,
+    author char(90),
+    mensagem char(255)
 );
